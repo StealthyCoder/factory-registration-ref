42db6d1e82b1cb1dbc0743434efd4491ac6dbf02
Eric Bode <eric.bode@foundries.io>
2022-12-08 16:08:25 +0100

    fix(misc): Aligning several fixes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,39 +1,40 @@
 import os
+
 from cryptography import x509
 from cryptography.x509.extensions import SubjectAlternativeName
-from cryptography.x509.oid import ObjectIdentifier
 from cryptography.x509.general_name import DNSName
+from cryptography.x509.oid import ObjectIdentifier
 from starlette.config import Config
 
-class _Settings():
-    
+
+class _Settings:
     def __init__(self):
         self.config = Config()
-        
+
     @property
     def CA_KEY(self):
-        if hasattr(self, '_ca_key'):
+        if hasattr(self, "_ca_key"):
             return self._ca_key
-        
-        with open(os.path.join(self.config('CERTS_DIR'), "local-ca.key"), "rb") as key:
+
+        with open(os.path.join(self.config("CERTS_DIR"), "local-ca.key"), "rb") as key:
             self._ca_key = key.read()
             return self._ca_key
 
     @property
     def CA_CRT(self):
-        if hasattr(self, '_ca_crt'):
+        if hasattr(self, "_ca_crt"):
             return self._ca_crt
-            
-        with open(os.path.join(self.config('CERTS_DIR'), "local-ca.pem"), "rb") as crt:
+
+        with open(os.path.join(self.config("CERTS_DIR"), "local-ca.pem"), "rb") as crt:
             self._ca_crt = crt.read()
             return self._ca_crt
 
     @property
     def ROOT_CRT(self):
-        if hasattr(self, '_root_crt'):
+        if hasattr(self, "_root_crt"):
             return self._root_crt
-            
-        with open(os.path.join(self.config('CERTS_DIR'), "factory_ca.pem"), "r") as crt:
+
+        with open(os.path.join(self.config("CERTS_DIR"), "factory_ca.pem"), "r") as crt:
             self._root_crt = crt.read()
             return self._root_crt
 
@@ -51,12 +52,14 @@ class _Settings():
 
     @property
     def DEVICE_GATEWAY_SERVER(self):
-        if hasattr(self, '_device_gateway_server'):
+        if hasattr(self, "_device_gateway_server"):
             return self._device_gateway_server
         if self.config("DEVICE_GATEWAY_SERVER", default="") == "":
-            with open(os.path.join(self.config('CERTS_DIR'), "tls-crt"), "rb") as crt:
+            with open(os.path.join(self.config("CERTS_DIR"), "tls-crt"), "rb") as crt:
                 certificate = x509.load_pem_x509_certificate(crt.read())
-                ext = certificate.extensions.get_extension_for_oid(ObjectIdentifier('2.5.29.17'))
+                ext = certificate.extensions.get_extension_for_oid(
+                    ObjectIdentifier("2.5.29.17")
+                )
                 value: SubjectAlternativeName = ext.value
                 values: List[str] = value.get_values_for_type(DNSName)
                 for name in values:
@@ -68,13 +71,14 @@ class _Settings():
 
     @property
     def OSTREE_SERVER(self):
-        if hasattr(self, '_ostree_server'):
+        if hasattr(self, "_ostree_server"):
             return self._ostree_server
-        ostree_hostname=$(openssl x509 -ext subjectAltName -noout -in ${CERTS_DIR}/tls-crt | grep -o "DNS:[^,]*ostree.foundries.io" | sed -e 's/^DNS://')
         if self.config("OSTREE_SERVER", default="") == "":
-            with open(os.path.join(self.config('CERTS_DIR'), "tls-crt"), "rb") as crt:
+            with open(os.path.join(self.config("CERTS_DIR"), "tls-crt"), "rb") as crt:
                 certificate = x509.load_pem_x509_certificate(crt.read())
-                ext = certificate.extensions.get_extension_for_oid(ObjectIdentifier('2.5.29.17'))
+                ext = certificate.extensions.get_extension_for_oid(
+                    ObjectIdentifier("2.5.29.17")
+                )
                 value: SubjectAlternativeName = ext.value
                 values: List[str] = value.get_values_for_type(DNSName)
                 for name in values:
